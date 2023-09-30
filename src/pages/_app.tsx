8b1799ccc1de051741eeeabe14583916575d57ce
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2023-09-30 22:30:33 +0900

    create a provider to manage the authentication state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
-import { getApp } from 'firebase/app'
+import { AuthProvider } from '@src/feature/auth/provider/AuthProvider'
 
 initializeFirebaseApp()
 function MyApp({ Component, pageProps }: AppProps) {
-  console.log(getApp())
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </ChakraProvider>
   )
 }
